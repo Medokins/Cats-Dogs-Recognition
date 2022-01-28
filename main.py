74362f873cf3aaa8d6ff2d613ab98d2f66c4cc86
Medokins <medokin_s@interia.pl>
2022-01-28 21:46:34 +0100

    add better model training and prediction

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from dataPreprocessing import preprocessData
+from dataPreprocessing import *
 
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
+from keras.models import Sequential
+from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from sklearn import random
 import numpy as np
 import pickle
+import time
 
+NAME = f"Cats-Dogs-Recognition-cnn-64x2{int(time.time())}"
+tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 
 # put path to your PetImages dir here
 DataSets = "C:\\Users\\nikod\\Desktop\\Cats-Dogs-Recognition\\PetImages"
 Categories = ["Dog", "Cat"]
 IMG_SIZE = 50  # to reshape images since images are in different shape
 overRidePickle = False
-
-if not overRidePickle:
-    X = pickle.load(open("X.pickle", "rb"))
-    y = pickle.load(open("y.pickle", "rb"))
+retrainModel = False
 
 if overRidePickle:
     trainingData = preprocessData(DataSets, Categories, IMG_SIZE)
@@ -42,27 +43,61 @@ if overRidePickle:
     pickle.dump(y, pickleOut)
     pickleOut.close()
 
-X = X/255.0
+if not overRidePickle:
+    X = pickle.load(open("X.pickle", "rb"))
+    y = pickle.load(open("y.pickle", "rb"))
 
-model = Sequential()
+if retrainModel:
 
-model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    X = X/255.0
 
-model.add(Conv2D(256, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    dense_layers = [1]  # originally 0, 1, 2
+    layer_sizes = [64]  # originally 32, 54, 128
+    conv_layers = [3]  # originally 1, 2, 3
 
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    for dense_layer in dense_layers:
+        for layer_size in layer_sizes:
+            for conv_layer in conv_layers:
+                NAME = "{}-conv-{}-nodes-{}-dense-{}".format(
+                    conv_layer, layer_size, dense_layer, int(time.time()))
 
-model.add(Dense(64))
+                model = Sequential()
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+                model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+                for l in range(conv_layer-1):
+                    model.add(Conv2D(layer_size, (3, 3)))
+                    model.add(Activation('relu'))
+                    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.fit(X, y, batch_size=32, epochs=5, validation_split=0.3)
+                model.add(Flatten())
+
+                for _ in range(dense_layer):
+                    model.add(Dense(layer_size))
+                    model.add(Activation('relu'))
+
+                model.add(Dense(1))
+                model.add(Activation('sigmoid'))
+
+                tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+                model.compile(loss='binary_crossentropy',
+                              optimizer='adam',
+                              metrics=['accuracy'],
+                              )
+
+                model.fit(X, y,
+                          batch_size=32,
+                          epochs=10,
+                          validation_split=0.3,
+                          callbacks=[tensorboard])
+
+    model.save("64x3-CNN.model")
+
+
+model = tf.keras.models.load_model("64x3-CNN.model")
+
+prediction = model.predict([prepare('testDoggo.jpg')])
+print(Categories[int(prediction[0][0])])
